e328d918fbb8bf70b683cd38abf1c18e942b4915
DerEins <mathieu.dupoux@bordeaux-inp.fr>
2022-12-05 21:13:38 +0100

    Add commentStat view

---
+++ sql/vues.sql
@@ -0,0 +1,36 @@
+-- ========================================
+--      Vue : CommentStat
+-- ========================================
+CREATE OR REPLACE VIEW commentStats AS (
+    SELECT bd.avis.numero_avis, bd.avis.note, 
+  when classement.nbUp IS NOT NULL 
+  then classement.nbUp
+  else 0
+  end as nbUp , 
+    CASE 
+    WHEN classement.indice IS NOT NULL 
+    THEN classement.indice
+    ELSE 0
+    END AS indice,bd.avis.date_avis, bd.avis.commentaire FROM bd.avis 
+    LEFT JOIN ( 
+        WITH c_table AS ( 
+            SELECT numero_avis,
+            CASE 
+            WHEN sum(pertinence)>=1 THEN sum(pertinence)
+            ELSE 0
+            END AS c FROM bd.appreciation
+            GROUP BY numero_avis), 
+        d_table AS (
+            SELECT numero_avis,
+            CASE 
+            WHEN sum(pertinence)<=-1 
+            THEN sum(pertinence)*-1
+            ELSE 0
+            END AS d
+            FROM bd.appreciation
+            GROUP BY numero_avis)
+        SELECT c_table.numero_avis, c_table.c+d_table.d AS nbUp, (1+c_table.c)/(1+d_table.d) AS indice
+        FROM c_table
+        INNER JOIN d_table ON c_table.numero_avis=d_table.numero_avis
+        GROUP BY c_table.numero_avis)
+    AS classement ON bd.avis.numero_avis=classement.numero_avis);
